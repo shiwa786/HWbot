21699104e431c3eef2aeb7affece061db78ecfa2
shiwa786 <74191978+shiwa786@users.noreply.github.com>
2022-10-12 11:54:00 +0530

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -53,33 +53,33 @@ class Config(object):
     #you can change these 
     INFOPIC=True #picture while doing /info
     STRICT_GBAN=True
-    API_ID=681 ##api id from my.telegram.org
-    APP_ID=6281 #same as API_ID
-    API_HASH="45aabfacadsdsds" ##api hash from my.telegram.org
-    APP_HASH="45aabfadsdsd" #same as API_HASH
+    API_ID= "4277083" ##api id from my.telegram.org
+    APP_ID= "4277083" #same as API_ID
+    API_HASH="bb0ddae0921fc020ce61faae2d1261d5" ##api hash from my.telegram.org
+    APP_HASH="bb0ddae0921fc020ce61faae2d1261d5" #same as API_HASH
     BL_CHATS=[1] #chats to be blacklisted
-    MONGO_DB_URL="mongodb+srv://mesdsdwhidsdsgodb.net/?retryWrites=true&w=majority" ##mongo database link (necessary)
+    MONGO_DB_URL="mongodb+srv://Alexabot:Alexabotdb@cluster0.6m9qtav.mongodb.net/?retryWrites=true&w=majority" ##mongo database link (necessary)
     DB_URL2="mongodb+srv://meowhisswswuj7.mongodb.net/?retryWrites=true&w=majority" #mongo db (not necessary)
-    DB_URL="postgresql://wdsdsw@ldsas/wasassddb" #postgres sql database link
+    DB_URL="postgresql://xrlqfbylexdtis:23d84c0772436747dfc4072806ac6c1f225577972d704340e9f641cf9b050a3e@ec2-3-213-66-35.compute-1.amazonaws.com:5432/dau8ap85onildp" #postgres sql database link
     REDIS_URL="redis://dsds3@reddsdsuth-1-1.ec2.cloud.redislabs.com:154dsd/Arssddsdsb" #redis database url from redislabs.com
-    TOKEN="54dssdV8e59Odo" #bot token from @BotFather
+    TOKEN="5643681960:AAHwNOqT9DFsxYDvCudTe2IOAkwjA_RghQQ" #bot token from @BotFather
     DEV_USERS=[5306064258] #developers id
     DRAGONS=[9656] #sudo users id
     DEMONS=[1909] #support user ids
     TIGERS=[1] #commas for multiple ids
     WOLVES=[2112, 1212] #commas for multiple ids 
     DONATION_LINK="https://www.paypal.me/PaulSonOfLars" #u can change with yours
-    EVENT_LOGS=-100159 #channel id for gban logs
-    JOIN_LOGGER=-1001523  #log channel/group id
-    OWNER_ID=5306064258 #owner id in integer
-    ERROR_LOGS=-1001 #support group id
-    BOT_NAME="Himawari" #your bot name
+    EVENT_LOGS=-1001747540803 #channel id for gban logs
+    JOIN_LOGGER=-1001747540803  #log channel/group id
+    OWNER_ID=5497627952 #owner id in integer
+    ERROR_LOGS=-1001747540803 #support group id
+    BOT_NAME="elisa" #your bot name
     ARQ_API_KEY="SLSFXSsdUXNSMH-ARQ" #ARQ api key from @ARQRobot
     ARQ_API_URL="arq.hamker.dev" #arq link
-    SUPPORT_CHAT="Ignidsn" #support group username without @
-    OWNER_USERNAME="arsshh" #owner username without @
-    UPDATES_CHANNEL="Igsd" #Updates/News Channel username without @
-    BOT_USERNAME="Himawari_Robot" #bot username without @
+    SUPPORT_CHAT="elisha_support" #support group username without @
+    OWNER_USERNAME="denvil_xdd" #owner username without @
+    UPDATES_CHANNEL="denvil_bots" #Updates/News Channel username without @
+    BOT_USERNAME="miselisarobot" #bot username without @
     REM_BG_API_KEY="K2dsdsYma6cZx" #not necessary
     GENIUS_API_TOKEN="e-8UdRQNrIssPyM" # api token from genius.com (not necessary)
     TIME_API_KEY="QLLLDV7SWFD3" #not necessary
